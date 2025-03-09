bdd24b0c0c5258c22bf37659b6cddf79ba528cdb
Skifak <skif7511@gmail.com>
2025-03-09 18:11:07 +0300

    one commit.

--- src/game/___tests___/damage.test.js
+++
@@ -1,9 +0,0 @@
-const { calculateDamage } = require("../damage");
-
-describe("calculateDamage", () => {
-    it("calculates damage correctly", () => {
-        const attacker = { strength: 10 };
-        const defender = { armor: 3 };
-        expect(calculateDamage(attacker, defender)).toBe(7);
-    });
-});
--- src/game/damage.js
+++
@@ -1,5 +0,0 @@
-function calculateDamage(attacker, defender) {
-    return attacker.strength - defender.armor;
-}
-
-module.exports = { calculateDamage };
