ef9c9682463c1c85f75fa5a64a9b125b628ab071
Skifak <skif7511@gmail.com>
2025-03-11 20:48:00 +0300

    fixdocker-compose and .env

--- volumes/db/01_init_auth.sql
+++ volumes/db/01_init_auth.sql
@@ -12,17 +12,36 @@ $do$;
 
 -- Создание схемы auth
 CREATE SCHEMA IF NOT EXISTS auth;
-GRANT USAGE ON SCHEMA auth TO authenticator;
+GRANT ALL ON SCHEMA auth TO postgres;
 
 -- Создание таблицы auth.users
 CREATE TABLE IF NOT EXISTS auth.users (
-    id uuid NOT NULL DEFAULT gen_random_uuid(),
-    email text NOT NULL UNIQUE,
-    encrypted_password text NOT NULL,
-    created_at timestamptz DEFAULT now(),
-    updated_at timestamptz DEFAULT now(),
-    last_sign_in_at timestamptz,
+    instance_id uuid NULL,
+    id uuid NOT NULL UNIQUE DEFAULT gen_random_uuid(),
+    aud varchar(255) NULL,
+    "role" varchar(255) NULL,
+    email varchar(255) NULL UNIQUE,
+    encrypted_password varchar(255) NULL,
+    confirmed_at timestamptz NULL,
+    invited_at timestamptz NULL,
+    confirmation_token varchar(255) NULL,
+    confirmation_sent_at timestamptz NULL,
+    recovery_token varchar(255) NULL,
+    recovery_sent_at timestamptz NULL,
+    email_change_token varchar(255) NULL,
+    email_change varchar(255) NULL,
+    email_change_sent_at timestamptz NULL,
+    last_sign_in_at timestamptz NULL,
+    raw_app_meta_data jsonb NULL,
+    raw_user_meta_data jsonb NULL,
+    is_super_admin bool NULL,
+    created_at timestamptz NULL DEFAULT now(),
+    updated_at timestamptz NULL DEFAULT now(),
     CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
-GRANT ALL ON auth.users TO authenticator;
+CREATE INDEX IF NOT EXISTS users_instance_id_email_idx ON auth.users USING btree (instance_id, email);
+CREATE INDEX IF NOT EXISTS users_instance_id_idx ON auth.users USING btree (instance_id);
+
+COMMENT ON TABLE auth.users IS 'Auth: Stores user login data within a secure schema.';
+GRANT ALL ON auth.users TO postgres;
